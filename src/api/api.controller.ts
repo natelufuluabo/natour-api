7afdf900c4a1e268af55c944324d462eb940bdf6
aircongo <natelufuluabo@yahoo.ca>
2023-12-03 08:57:30 -0500

    implemented filering by region

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Query } from '@nestjs/common';
 import { ApiService, tour } from './api.service';
 
 @Controller('api')
 export class ApiController {
-    constructor(private readonly apiService: ApiService) {}
+  constructor(private readonly apiService: ApiService) {}
 
-    @Get()
-    @HttpCode(200)
-    getAllTours(): tour[] {
-        return this.apiService.getAllTours();
-    }
+  @Get()
+  @HttpCode(200)
+  getAllTours(@Query() params: any): tour[] {
+    if (params.region) return this.apiService.getTourByRegion(params.region);
+    return this.apiService.getAllTours();
+  }
 
-    @Get(':id')
-    @HttpCode(200)
-    getOneTour(@Param('id') id: string): tour {
-        return this.apiService.getOneTour(Number(id));
-    }
+  @Get(':id')
+  @HttpCode(200)
+  getOneTour(@Param('id') id: string): tour {
+    return this.apiService.getOneTour(Number(id));
+  }
 }
--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,63 +1,73 @@
 import { Injectable } from '@nestjs/common';
 
 export type tour = {
-    id: number;
-    name: string;
-    destination: string;
-    departure_date: string;
-    duration: number;
-    price: number;
-    description: string;
-}
+  id: number;
+  name: string;
+  destination: string;
+  departure_date: string;
+  duration: number;
+  price: number;
+  description: string;
+  region: string;
+};
 
 const tours: tour[] = [
-    {
-      "id": 101,
-      "name": "Adventure in the Amazon",
-      "destination": "Amazon Rainforest",
-      "departure_date": "2023-05-15",
-      "duration": 10,
-      "price": 2500,
-      "description": "Embark on a thrilling adventure in the heart of the Amazon Rainforest. Explore diverse ecosystems, encounter unique wildlife, and experience the wonders of this vibrant and lush environment."
-    },
-    {
-      "id": 102,
-      "name": "European Discovery",
-      "destination": "Paris, Rome, Barcelona",
-      "departure_date": "2023-07-10",
-      "duration": 14,
-      "price": 3800,
-      "description": "Immerse yourself in the rich history and culture of Europe as you visit iconic cities like Paris, Rome, and Barcelona. Enjoy breathtaking architecture, delicious cuisine, and the charm of each unique destination."
-    },
-    {
-      "id": 103,
-      "name": "Tropical Paradise Escape",
-      "destination": "Bali, Maldives, Seychelles",
-      "departure_date": "2023-09-25",
-      "duration": 12,
-      "price": 5200,
-      "description": "Relax and rejuvenate in tropical paradise! Unwind on the beautiful beaches of Bali, indulge in the luxury of the Maldives, and explore the pristine beauty of Seychelles. A perfect getaway for sun-seekers."
-    },
-    {
-      "id": 104,
-      "name": "Cultural Expedition in Japan",
-      "destination": "Tokyo, Kyoto, Osaka",
-      "departure_date": "2023-06-08",
-      "duration": 9,
-      "price": 4000,
-      "description": "Immerse yourself in the fascinating culture of Japan. Visit bustling Tokyo, explore the historic streets of Kyoto, and savor the culinary delights of Osaka. A journey through tradition and modernity."
-    },
-    {
-      "id": 105,
-      "name": "Safari Adventure in Africa",
-      "destination": "Serengeti National Park, Maasai Mara",
-      "departure_date": "2023-08-20",
-      "duration": 7,
-      "price": 6500,
-      "description": "Experience the thrill of a safari in the vast landscapes of Serengeti and Maasai Mara. Witness the incredible wildlife, including the Big Five, and enjoy the beauty of the African savannah."
-    }
-]
-  
+  {
+    id: 101,
+    name: 'Adventure in the Amazon',
+    destination: 'Amazon Rainforest',
+    departure_date: '2023-05-15',
+    duration: 10,
+    price: 2500,
+    description:
+      'Embark on a thrilling adventure in the heart of the Amazon Rainforest. Explore diverse ecosystems, encounter unique wildlife, and experience the wonders of this vibrant and lush environment.',
+    region: 'Americas',
+  },
+  {
+    id: 102,
+    name: 'European Discovery',
+    destination: 'Paris, Rome, Barcelona',
+    departure_date: '2023-07-10',
+    duration: 14,
+    price: 3800,
+    description:
+      'Immerse yourself in the rich history and culture of Europe as you visit iconic cities like Paris, Rome, and Barcelona. Enjoy breathtaking architecture, delicious cuisine, and the charm of each unique destination.',
+    region: 'Europe',
+  },
+  {
+    id: 103,
+    name: 'Tropical Paradise Escape',
+    destination: 'Bali, Maldives, Seychelles',
+    departure_date: '2023-09-25',
+    duration: 12,
+    price: 5200,
+    description:
+      'Relax and rejuvenate in tropical paradise! Unwind on the beautiful beaches of Bali, indulge in the luxury of the Maldives, and explore the pristine beauty of Seychelles. A perfect getaway for sun-seekers.',
+    region: 'Asia',
+  },
+  {
+    id: 104,
+    name: 'Cultural Expedition in Japan',
+    destination: 'Tokyo, Kyoto, Osaka',
+    departure_date: '2023-06-08',
+    duration: 9,
+    price: 4000,
+    description:
+      'Immerse yourself in the fascinating culture of Japan. Visit bustling Tokyo, explore the historic streets of Kyoto, and savor the culinary delights of Osaka. A journey through tradition and modernity.',
+    region: 'Asia',
+  },
+  {
+    id: 105,
+    name: 'Safari Adventure in Africa',
+    destination: 'Serengeti National Park, Maasai Mara',
+    departure_date: '2023-08-20',
+    duration: 7,
+    price: 6500,
+    description:
+      'Experience the thrill of a safari in the vast landscapes of Serengeti and Maasai Mara. Witness the incredible wildlife, including the Big Five, and enjoy the beauty of the African savannah.',
+    region: 'Africa',
+  },
+];
 
 @Injectable()
 export class ApiService {
@@ -65,7 +75,11 @@ export class ApiService {
     return tours;
   }
 
-  getOneTour(id:number): tour {
-    return tours.find(tour => tour.id === id);
+  getOneTour(id: number): tour {
+    return tours.find((tour) => tour.id === id);
+  }
+
+  getTourByRegion(region: string): tour[] {
+    return tours.filter((tour) => tour.region === region);
   }
 }
